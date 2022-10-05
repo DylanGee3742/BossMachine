f96055ad9ae4de471429ddbe9beab5028465c791
Dylan Gee <dylangee@gmail.com>
2022-10-05 20:21:14 +0100

    function for checking valid input

--- server/functions.js
+++ server/functions.js
@@ -1,8 +1,7 @@
 const isMinionValid = (minion) => {
     let isValid;
-    if (
-        (minion.name && typeof minion.name === 'string') &&
-        (minion.title && typeof minion.title === 'string') && 
+    if  ((minion.name && typeof minion.name === 'string') &&
+        (minion.title && typeof minion.title === 'string') &&
         (minion.weaknesses && typeof minion.weaknesses === 'string') &&
         (minion.salary && typeof minion.salary === 'number'))
      {
