60a1c3b8cfbc83b955a7ae6de82582c6da50ff70
Dylan Gee <dylangee@gmail.com>
2022-10-06 09:14:57 +0100

    Import and use of meetings Router

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ module.exports = app;
 /* Do not change the following line! It is required for testing and allowing
 *  the frontend application to interact as planned with the api server
 */
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4000;
 
 // Add middleware for handling CORS requests from index.html
 app.use(cors())
@@ -22,10 +22,12 @@ app.use('/api/minions', apiMinionRouter);
 const apiIdeasRouter = require('./server/apiIdeas')
 app.use('/api/ideas', apiIdeasRouter)
 
+const apiMeetingsRouter = require('./server/apiMeetings')
+app.use('/api/meetings', apiMeetingsRouter)
+
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
   app.listen(PORT, () => {
   })
 }
-
